1119a776915c1f109331e3c06b075103ba26e893
lawsonredeye <omoregbeeolawson@gmail.com>
2024-07-04 20:45:01 +0100

    (fix): pycodestyle fix, added new line after module comment

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Module that has a function which converts a float into a string"""
 
+
 def to_str(n: float) -> str:
     """Converts a float point number into a string via Typecasting"""
     return str(n)
