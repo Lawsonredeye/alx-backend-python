8661b32746e726515b7d23c9e0dd617cac99968a
lawsonredeye <omoregbeeolawson@gmail.com>
2024-07-04 22:33:11 +0100

    (fix): pycodestyle

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
-"""function sum_list which takes a list input_list of floats 
+"""function sum_list which takes a list input_list of floats
 as argument and returns their sum as a float."""
 
 from typing import List
 
+
 def sum_list(input_list: List[float]) -> float:
     """Takes a list of float numbers and sum it up and returns a
     float value"""
-
     count: float = 0
     for i in input_list:
         count += i
