123fdb52aaed7ed6bc3c1e91e481f248a6f9cc75
lawsonredeye <omoregbeeolawson@gmail.com>
2024-07-04 22:26:18 +0100

    complex annotations

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""function sum_mixed_list which takes a list mxd_lst of integers
+and floats and returns their sum as a float."""
+from typing import Union, List
+
+
+def sum_mixed_list(mxd_lst: List[Union[int | float]]) -> float:
+    """takes a list mxd_lst of integers
+    and floats and returns their sum as a float.
+    """
+    count: float = 0
+    for i in mxd_lst:
+        count += i
+    return count
