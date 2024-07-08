f8c2b2ca10fabf7259f9ddf44ea955c789a47346
lawsonredeye <omoregbeeolawson@gmail.com>
2024-07-09 00:58:00 +0100

    (fix): pycodestyle

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -5,6 +5,7 @@ import asyncio
 import random
 import typing
 
+
 async def wait_random(max_delay: typing.Union[int, float] = 10):
     '''Basic usage of the async library to perform a simple async operation'''
     rand_sec = random.uniform(0, max_delay)
