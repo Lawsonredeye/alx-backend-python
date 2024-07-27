137c978788bf59b770c9ee141e0c100604aedfaf
Lawsonredeye <omoregbeeolawson@gmail.com>
2024-07-28 00:19:54 +0100

    (mod): exec

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,6 +5,7 @@ unittest
 
 from utils import access_nested_map
 import unittest
+from parameterized import parameterized
 
 
 nested_val = {"some": {"thing": 1}}
@@ -14,8 +15,11 @@ print(access_nested_map(nested_val, nested_val))
 class TestAccessNestedMap(unittest.testCase):
     """Testing the nested_map function and its return values"""
 
-    @parameterized.expand(nested_map={"a": 1}, path=("a",))
-    @parameterized.expand(nested_map={"a": 1}, path=("a",))
+    @parameterized.expand(
+        nested_map={"a": 1}, path=("a",),
+        nested_map={"a": {"b": 2}}, path=("a",),
+        nested_map={"a": {"b": 2}}, path=("a", "b")
+        )
     def test_access_nested_map(self):
         """chc"""
         self.assertEqual()
