62ec10a83f10881477b5d1b736406f3952efd7ba
Lawsonredeye <omoregbeeolawson@gmail.com>
2024-07-29 02:11:47 +0100

    test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -20,6 +20,17 @@ class TestAccessNestedMap(unittest.TestCase):
         """check if the output is correct"""
         self.assertEqual(access_nested_map(input, expected), output)
 
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b")),
+    ])
+    def test_access_map_exception(self, nest_map, path):
+        """Testing for invalid parameters that doesnt match the key or value"""
+        with self.assertRaises(KeyError) as err:
+            access_nested_map(nest_map, path)
+        
+        self.assertEqual(err.exception.args[0], path[-1])
+
 
 if __name__ == "__main__":
     unittest.main()
